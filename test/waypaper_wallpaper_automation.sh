c6d67459f58152aa7737947ecf6ad85bfa419964
ScorpioGK <scorpiogameking@gmail.com>
2026-06-16 15:16:52 -0400

    Update waypaper_wallpaper_automation.sh

--- test/waypaper_wallpaper_automation.sh
+++ test/waypaper_wallpaper_automation.sh
@@ -2,7 +2,6 @@
 # The following is am example automation
 # script to for creating and setting the 
 # generated image to a waypaper wallpaper
-# - Scorpio
 # ---------------------------------------
-python main.py -t "Just Another $(date +%A)" -f "cosmic" -s 30 -w 100 -c "#ebdbb2" -b "#282828" -n 'day_of_week.png' -o '~/Pictures/Backgrounds/Static/Asciiwall/'
+python main.py -t "Just Another $(date +%A)" -f "random" -s 30 -w 100 -c "#ebdbb2" -b "#282828" -n 'day_of_week.png' -o '~/Pictures/Backgrounds/Static/Asciiwall/'
 waypaper --wallpaper './day_of_week.png'
